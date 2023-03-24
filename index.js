b5ec0a5d611e9280e55c1e1cb272bb7983a52fa2
Aman <Sharmaamqn@gmail.com>
2023-03-25 00:06:13 +0530

    use bcrypt to hash password and also save a new user to database

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-const express = require('express');
-const User = require('./models/user');
+const express = require("express");
+const User = require("./models/user");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
-app.set('view engine','ejs');
-app.set('views','views');
+mongoose
+  .connect("mongodb://127.0.0.1:27017/authDemo")
+  .then(() => {
+    console.log("Mongo connection open");
+  })
+  .catch((err) => {
+    console.log("Something went wrong");
+    console.log(err);
+  });
 
-app.get('/register',(req,res)=>{
-    res.render('register')
-})
-app.get('/secret',(req,res)=>{
-    res.send('This is a secret');
-})
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.listen(3000,()=>{
-    console.log("Serving");
-})
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("This is the home page");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.post("/register", async (req, res) => {
+  const { password, username } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({
+    username,
+    password: hash,
+  });
+  await user.save();
+  res.redirect("/");
+});
+app.get("/secret", (req, res) => {
+  res.send("This is a secret");
+});
+
+app.listen(3000, () => {
+  console.log("Serving");
+});
